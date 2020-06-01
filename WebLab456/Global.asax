fc3da455042750f38b32af19c345abba6a784b2d
Công Trọng <trongcongnguyen21101999@gmail.com>
2020-06-02 01:25:46 +0700

    Add project files.

---
+++ WebLab456/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebLab456.MvcApplication" Language="C#" %>
---
+++ WebLab456/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebLab456.Startup))]
+namespace WebLab456
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
