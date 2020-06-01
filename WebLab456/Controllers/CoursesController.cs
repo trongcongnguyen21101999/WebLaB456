9daf07af260b29b573332e1549974d8f80448348
Công Trọng <trongcongnguyen21101999@gmail.com>
2020-06-02 02:08:46 +0700

    lần 2-trang 20-Controllers

---
+++ WebLab456/Controllers/CoursesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebLab456.Models;
+using WebLab456.ViewModels;
+
+namespace WebLab456.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create()
+        {
+            var ViewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(ViewModel);
+        }
+    }
+}
---
+++ WebLab456/ViewModels/CourseViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebLab456.Models;
+
+namespace WebLab456.ViewModels
+{
+    public class CourseViewModel
+    {
+        public string Place { get; set; }
+
+        public string Date { get; set; }
+
+        public string Time { get; set; }
+
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0}  {1}", Date, Time));
+        }
+    }
+}
