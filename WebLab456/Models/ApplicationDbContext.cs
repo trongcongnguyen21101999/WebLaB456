9dcd95d50be984b0cb9ab3314f7f69e5efff22b7
Công Trọng <trongcongnguyen21101999@gmail.com>
2020-06-11 02:30:48 +0700

    lần 6-Sử dụng công cụ Postman để Test API

--- WebLab456/Models/ApplicationDbContext.cs
+++ WebLab456/Models/ApplicationDbContext.cs
@@ -10,8 +10,8 @@ namespace WebLab456.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Course> Courses { get; set; }
-
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
@@ -21,6 +21,15 @@ namespace WebLab456.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Course)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
--- WebLab456/Models/Attendance.cs
+++ WebLab456/Models/Attendance.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -9,8 +11,14 @@ namespace WebLab456.Models
     {
         public Course Course { get; set; }
 
-        [key]
-        [Column(OrderedParallelQuery = 1]
+        [Key]
+        [Column(Order = 1)]
+        public int CourseId { get; set; }
 
+        public ApplicationUser Attendee { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        public string AttendeeId { get; set; }
     }
 }
